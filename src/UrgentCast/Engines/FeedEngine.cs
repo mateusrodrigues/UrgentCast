2346f2343a4cdf1ccbef64cd3deb66fe531074f8
Mateus Rodrigues de Morais <mateus.rodrigues@live.com>
2016-10-20 19:04:15 -0300

    Feed generation empty class

---
+++ src/UrgentCast/Engines/FeedEngine.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UrgentCast.Engines
+{
+    public class FeedEngine
+    {
+        public FeedEngine()
+        {
+        }
+
+        public void GenerateFeed()
+        {
+            
+        }
+    }
+}
